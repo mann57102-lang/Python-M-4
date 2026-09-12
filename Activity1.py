83f240abb667f029af5f403e904654bac915f5d5
mann57102-lang <mann57102@gmail.com>
2026-09-12 20:16:56 +0530

    Create Activity1.py

---
+++ Activity1.py
@@ -0,0 +1,16 @@
+# Create an empty list
+blank_list = []
+print()
+
+# A list of numbers
+values = [10, 20, 30, 40, 50]
+print(values)
+
+# Use * operator
+repeated_values = [4, 5, 6] * 3
+print(repeated_values)
+
+# Reverse the given list
+my_list = [600, 700, 800, 900, 1000]
+my_list = my_list[::-1]
+print(my_list, "\n")
